229bf5cbc592a083a4c203c505da39ecf1e84230
Cristian Carbajal <cristian.carbajal@sovos.com>
2019-11-12 21:23:30 -0300

    Deleted scipted link which prevented popup

--- sprint2/index.php
+++ sprint2/index.php
@@ -194,7 +194,6 @@
   ?>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
-<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
