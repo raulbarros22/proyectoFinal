5d587cd0211eedfaeda925bd34a1682df7ea5503
Cristian Carbajal <cristian.carbajal@sovos.com>
2019-12-16 10:24:42 -0300

    Creates a cart when a user is registered

--- db_GoodBuy+tablas_login_laravel.sql
+++ db_GoodBuy+tablas_login_laravel.sql
@@ -22,7 +22,7 @@ ADD COLUMN `RAM` VARCHAR(15) NULL DEFAULT 'No especificado' AFTER `camara`,
 ADD COLUMN `OS` VARCHAR(15) NULL DEFAULT 'No especificado' AFTER `RAM`;
 
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -34,9 +34,12 @@ CREATE TABLE `users` (
   `telefono` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `address` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `dni` int(11) DEFAULT NULL,
+  `cart_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `users_email_unique` (`email`),
+  KEY `cart_id_idx` (`cart_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE `password_resets` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -46,28 +49,17 @@ CREATE TABLE `password_resets` (
   KEY `password_resets_token_index` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `goodbuy_db`.`carts` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
-  `total_price` DOUBLE NOT NULL DEFAULT 0,
-  `subtotal` DOUBLE NOT NULL DEFAULT 0,
-  `discounts` DOUBLE NOT NULL DEFAULT 0,
-  `shipping_cost` DOUBLE NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
-ALTER TABLE `goodbuy_db`.`carts` 
-ADD INDEX `cart_user_idx` (`user_id` ASC),
-ADD CONSTRAINT `cart_user`
-  FOREIGN KEY (`id`)
-  REFERENCES `goodbuy_db`.`users` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+CREATE TABLE `carts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp(),
+  `product_id` int DEFAULT NULL,
+  `total_price` double NOT NULL DEFAULT 0,
+  `subtotal` double NOT NULL DEFAULT 0,
+  `discounts` double NOT NULL DEFAULT 0,
+  `shipping_cost` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
   
 CREATE TABLE `goodbuy_db`.`cart_product` (
   `id` INT NOT NULL AUTO_INCREMENT,
