3aafe9fe8bf1f81ab7df547ce6f3f2ed9dfc437d
raulbarros22 <barrosraul22@outlook.com>
2019-12-19 18:53:48 -0300

    Power point

--- GoodBuy-Laravel/routes/web.php
+++ GoodBuy-Laravel/routes/web.php
@@ -19,33 +19,33 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/',function(){
+Route::get('',function(){
   return view('index');
 });
-Route::get('/faq',function(){
+Route::get('faq',function(){
   return view('faq');
 });
-Route::get('/contact',function(){
+Route::get('contact',function(){
   return view('contact');
 });
-Route::get('/carrito', 'CartController@search')->middleware('auth');
-Route::get('/profile',function(){
+Route::get('carrito', 'CartController@search')->middleware('auth');
+Route::get('profile',function(){
   return view('profile');
 })->middleware('auth');
 
-Route::get('/confirmacion', 'CartController@finishOrder')->middleware('auth');
-Route::get('/abmProducts', 'ProductController@abm')->middleware('auth');
-Route::get('/celulares', 'ProductController@list');
-Route::get('/notebooks', 'ProductController@list');
-Route::get('/product/{id}', 'ProductController@detail');
-Route::post('/addToCart/{id}', 'CartController@addToCart')->middleware('auth');
-Route::post('/delete/{id}', 'CartController@deleteFromCart')->middleware('auth');
-Route::post('/agregar','ProductController@store');
-Route::get('/modificar','ProductController@search');
-Route::get('/modificar/{id}','ProductController@modify');
-Route::put('/modificar','ProductController@update');
-Route::delete('/borrar','ProductController@destroy');
-Route::get('/exito/{r}',function($r){
+Route::get('confirmacion', 'CartController@finishOrder')->middleware('auth');
+Route::get('abmProducts', 'ProductController@abm')->middleware('auth');
+Route::get('celulares', 'ProductController@list');
+Route::get('notebooks', 'ProductController@list');
+Route::get('product/{id}', 'ProductController@detail');
+Route::post('addToCart/{id}', 'CartController@addToCart')->middleware('auth');
+Route::post('delete/{id}', 'CartController@deleteFromCart')->middleware('auth');
+Route::post('agregar','ProductController@store');
+Route::get('modificar','ProductController@search');
+Route::get('modificar/{id}','ProductController@modify');
+Route::put('modificar','ProductController@update');
+Route::delete('borrar','ProductController@destroy');
+Route::get('exito/{r}',function($r){
   $vac=compact('r');
   return view('exito',$vac);
 });
